cbf51ac1fe69248e2266606a0b0e948ea4e66a7a
Johnatan Andrés Arango R <ingyotis@gmail.com>
2021-01-27 23:13:51 -0500

    workshop 1 terminado. Homepage de la tiendita Avocado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,55 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// //conectarnos al servidor
+// window.fetch(url).
+// // procesar la respuesta y convertirla en JSON
+// then(respuesta => {respuesta.json()})
+// // pasar el JSON a información a renderizar
+// .then(responseJson => {
+//   responseJson.data.forEach( (item) => {
+//     // crear una imagen, un título y el precio del elemento
+//     const image = document.createElement('img')
+//     const title = document.createElement('h2')
+//     const price = document.createElement('div')
 
-console.log('Happy hacking :)')
+//   })
+// })
+
+const BASE_URL = "https://platzi-avo.vercel.app"
+const container = document.querySelector('.contenido')
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat('en-EN', { 
+    style: 'currency', 
+    currency: 'USD'
+  }).format(price)
+  return newPrice
+}
+
+function renderProduct (image, title, price, description){
+  const itemBox = `
+    <div class="item-container flex bg-white shadow-lg rounded-lg overflow-hidden">
+      <img class="w-1/3" src="${BASE_URL}${image}">     
+      <div class="text-gray-900 text-2xl">
+        <p class="description mt-2 text-gray-600 text-sm">${description}</p>
+        <h2 class="title">${title}</h2>
+        <div class="price flex item-center justify-between mt-3">
+          <p class="mt-2 text-gray-600 text-sm">${formatPrice(price)}</p>
+          <button class="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">
+            Añadir al Carrito
+          </button>        
+        </div>
+      </div>
+    </div>
+  `
+  return itemBox
+}
+
+async function obtenerData(){
+  let contenido = ''
+  const respuesta = await fetch(`${BASE_URL}/api/avo`)
+  const responseJson = await respuesta.json()
+  responseJson.data.forEach( (item) => { 
+    contenido += renderProduct(item.image, item.name, item.price, item.attributes.description) 
+  })
+  container.innerHTML = contenido
+}
+
+obtenerData()
